3665fc2df09b8fa048435345b2c84ca9160dc33d
Sheikh Zubair Ahmed <134803474+zubairahmed23@users.noreply.github.com>
2023-11-21 20:44:45 +0530

    commit done

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+const PORT = 4000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.listen(PORT, err => {
+    if(err){
+        console.log(err.message);
+    }
+});
+
+app.get('/', (req, res) => {
+    res.redirect('/main');
+});
+
+app.get('/main', (req, res) => {
+    res.render('home');
+});
