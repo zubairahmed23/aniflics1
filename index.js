999e87e16fc805a5188c30bacd6f806d7b27721e
Sheikh Zubair Ahmed <134803474+zubairahmed23@users.noreply.github.com>
2023-11-22 16:52:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 4000;
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname+'/../views');
+app.set('views', __dirname+'./views');
 
 app.listen(PORT)
 /* Hello */
